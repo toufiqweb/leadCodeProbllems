b5564ee9528ce10c52e8801f1dc6513e036f2c1c
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-07-05 16:02:26 +0600

    Add compose function to apply a series of transformations

---
+++ 2629.js
@@ -0,0 +1,17 @@
+var compose = function(functions) {
+    
+    // console.log(functions);
+    
+    return function(x) {
+        
+        let result = x
+
+        for (let i = functions.length; i >= 0 ; i--) {
+             result = functions[i](result);
+            
+        }
+    }
+};
+
+
+console.log(functions = [x => x + 1, x => x * x, x => 2 * x], x = 4);

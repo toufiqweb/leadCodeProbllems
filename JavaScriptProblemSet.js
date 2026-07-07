14d963e8502b9ba87a19ff530de6969243faaa15
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-07-07 13:51:33 +0600

    checkSign

--- JavaScriptProblemSet.js
+++ JavaScriptProblemSet.js
@@ -59,4 +59,26 @@ function CelsiusToFahrenheit(c) {
   return f;
 }
 
-console.log(CelsiusToFahrenheit(0));
+// console.log(CelsiusToFahrenheit(0));
+
+
+// Problem 5: Check Positive, Negative or Zero  [Easy]
+// Description: Write a function checkSign(n) that returns 'positive', 'negative', or 'zero' based on the value of n.
+// Example:
+// Input: -5  → Output: 'negative'Input: 0   → Output: 'zero'
+// Hint: Use if-else if-else statements.
+
+function checkSign(n) {
+  
+  if (n > 0) {
+    return "positive"
+  }else if (n < 0) {
+    return "negative"
+  }
+
+  if (n === 0) {
+    return "zero"
+  }
+}
+
+console.log(checkSign(1));

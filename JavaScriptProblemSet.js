1e66cd32d41dff2e91e01ad406387e7663349d7a
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-07-09 16:07:03 +0600

    reverseString

--- JavaScriptProblemSet.js
+++ JavaScriptProblemSet.js
@@ -121,3 +121,18 @@ function processData(input) {
 } 
 
 processData("10 25")
+
+
+// Problem 6: Reverse a String  [Easy]
+// Description: Write a function reverseString(str) that returns the reverse of a given string.
+// Example:
+// Input: 'hello'   → Output: 'olleh'Input: 'world'   → Output: 'dlrow'
+// Hint: Use split(''), reverse(), and join('').
+
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+
+console.log(reverseString("hello")); 
+console.log(reverseString("world")); 
